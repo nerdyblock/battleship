ac5efcb19b58e30a95cfdb602d6d53319bfb9c77
Akash Pal <creativeakashpal@gmail.com>
2023-04-14 22:12:23 +0530

    added test for generate board fn

--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -1,8 +1,8 @@
-const player = require("../modules/createPlayer");
+// const player = require("../modules/createPlayer");
 const gameboard = require("../modules/gameboard");
 
 describe("randomly generate ship", () => {
-    const newPlayer = player();
+    const newPlayer = gameboard();
     
     it("generate a ship of length 4", () => {
         const newRandomShip = newPlayer.createRandomShipLocation(4);
@@ -10,6 +10,14 @@ describe("randomly generate ship", () => {
     });
 });
 
+describe("randomly generate gameboard", () => {
+    const newPlayer = gameboard();
+    newPlayer.randomBoardGenerator();
+    
+    it("board has 8 ships", () => {
+        expect(newPlayer.ships.length).toBe(8)
+    });
+});
 // it("validation for randomly generated ship", () => {
 //     const newPlayer = player();
 
