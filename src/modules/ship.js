1de8a9fbc4801b327e6edc5dee88427841a2d831
Akash Pal <creativeakashpal@gmail.com>
2023-05-27 20:24:49 +0530

    added getDirection to show direction of ship

--- src/modules/ship.js
+++ src/modules/ship.js
@@ -3,6 +3,15 @@ const ship = (pos) => {
         pos,
         length: pos.length,
         hits : 0,
+        getDirection() {
+            let direction;
+            if (pos.length > 1) {
+                direction = pos[0][0] === pos[1][0] ?
+                'x' : 'y';
+            } else { direction = 'x'; }
+
+            return direction;
+        },
         hit () {
             if(this.isSunk)
                 this.hits += 1;
