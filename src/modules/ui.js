630f8c552722fb403cfe594fe31c320231d52818
Akash Pal <creativeakashpal@gmail.com>
2023-07-07 11:31:23 +0530

    added feature to show ship count and winner

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -9,7 +9,9 @@ export const createBoardUi = (playerName) => {
     
     const board = createCells();
     board.classList.add(`${playerName}`)
-    gameboard.appendChild(board);
+    // gameboard.appendChild(board);
+
+    gameboard.insertBefore(board, gameboard.firstChild);
 
     return gameboard;
 }
@@ -41,19 +43,6 @@ export const uiFire = (firedLocation, attackMsg) => {
     }
 }
 
-// export const uiRemoveShip = (ship, playerName) => {
-//     ship.pos.forEach((cell, index) => {
-//         const shipCell = cell.join(',');
-//         const uiCell = document.querySelector(`.${playerName} [data-location="${shipCell}"]`);
-
-//         if(index === 0) {
-//             uiCell.innerHTML = '';
-//         }
-
-//         uiCell.classList.remove('ship-cell');        
-//     })
-// }
-
 export const uiRemoveshipCell = (playerName) => {
     const shipCells = document.querySelectorAll(`.${playerName} .ship-cell`);
     shipCells.forEach(item => {
@@ -64,19 +53,22 @@ export const uiRemoveshipCell = (playerName) => {
 function shipOverLay(ship, uiCell) {
     const direction = ship.getDirection();
 
-    let width;
-    let height;
+    let width = 2;
+    let height = 2;
     
     if(direction === 'x')  {
-        width = ship.length*40;
-        height = 40;
+        width = ship.length*2;
     } else {
-        width = 40;
-        height = ship.length*40;
+        height = ship.length*2;
     }
 
-    // uiCell.innerHTML = `<div id="${Math.random().toString(16).slice(2)}" draggable="true" data-length="${ship.length}" data-position="${direction}" class="ship-box" style="width: ${width}px;height: ${height}px;"></div>`
-    uiCell.innerHTML = `<div id="${Math.random().toString(16).slice(2)}" data-length="${ship.length}" data-position="${direction}" class="ship-box" style="width: ${width}px;height: ${height}px;"></div>`
+    uiCell.innerHTML = `<div id="${Math.random().toString(16).slice(2)}" data-length="${ship.length}" data-position="${direction}" class="ship-box" style="width: ${width}em;height: ${height}em;"></div>`
+}
+
+export function uiShowSunkShipCount(playerName) {
+    const shipCountDiv = document.querySelector(`.${playerName}-ship-count .ship-count`);
+    const prevShipCount = shipCountDiv.textContent;
+    shipCountDiv.textContent = prevShipCount - 1;
 }
 
 export function uiShowSunkShip(ship, playerName) {
@@ -93,23 +85,8 @@ export const uiPlaceShip = (ship, playerName) => {
         const uiCell = document.querySelector(`.${playerName} [data-location="${shipCell}"]`);
 
         if(index === 0) {
-            
             shipOverLay(ship, uiCell);
             uiCell.firstChild.setAttribute('draggable', true);
-            // const direction = ship.getDirection();
-
-            // let width;
-            // let height;
-            
-            // if(direction === 'x')  {
-            //     width = ship.length*40;
-            //     height = 40;
-            // } else {
-            //     width = 40;
-            //     height = ship.length*40;
-            // }
-
-            // uiCell.innerHTML = `<div id="${Math.random().toString(16).slice(2)}" draggable="true" data-length="${ship.length}" data-position="${direction}" class="ship-box" style="width: ${width}px;height: ${height}px;"></div>`
         }
         
         uiCell.classList.add('ship-cell');
@@ -128,4 +105,15 @@ export const uiClearBoard = () => {
         item.innerHTML = "";
         item.classList.remove('ship-cell');
     })
+}
+
+export function uiShowWinner(winner) {
+    const winnerModal = document.querySelector('dialog');
+    const winnerMsg = document.querySelector('dialog .winner-msg')
+    if(winner === 'player1') {
+      winnerMsg.textContent = 'YOU WON!!';
+    } else {
+      winnerMsg.textContent = 'YOU LOSE!!';
+    }
+    winnerModal.showModal();
 }
