5e859bc55ea9a409fd9e0a8d2c452f74129de035
Akash Pal <creativeakashpal@gmail.com>
2023-07-07 14:27:36 +0530

    added test for player name & game over

--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -1,30 +1,56 @@
 const player = require("../modules/player");
-const gameboard = require("../modules/gameboard");
 
-describe("randomly generate gameboard", () => {
-    const newPlayer = gameboard();
-    newPlayer.randomBoardGenerator();
-    
-    it("board has 8 ships", () => {
-        expect(newPlayer.ships.length).toBe(5)
+describe("player name", () => {
+    const newPlayer = player('player1');
+
+    it("get player name", () => {
+        expect(newPlayer.getName()).toBe('player1');
     });
+
+    it("set player name", () => {
+        newPlayer.setName('nerdy');
+        expect(newPlayer.getName()).toBe('nerdy');
+    })
 });
 
 describe("fire enemy ship", () => {
-    const newBoard = gameboard();
     const newPlayer = player();
-    newBoard.placeShip([[0,0]]);
+    newPlayer.board.placeShip([[0,0]]);
 
     it("missed enemy ship", () => {
-        newPlayer.fire(newBoard, [1,0]);
-        expect(newBoard.ships[0].hits).toBe(0);
-        expect(newBoard.missedShots[0]).toStrictEqual([1, 0]);
+        newPlayer.fire([1,0]);
+        expect(newPlayer.board.ships[0].hits).toBe(0);
+        expect(newPlayer.board.missedShots[0]).toStrictEqual([1, 0]);
     })
 
     it("hit the enemy ship", () => {
-        newPlayer.fire(newBoard, [0,0]);
-        expect(newBoard.ships[0].hits).toBe(1);
-        expect(newBoard.damageShips[0]).toStrictEqual([0,0]);
+        newPlayer.fire([0,0]);
+        expect(newPlayer.board.ships[0].hits).toBe(1);
+        expect(newPlayer.board.damageShips[0]).toStrictEqual([0,0]);
     })
 
+    it("is game over", () => {
+        expect(newPlayer.isGameOver()).toBe(true);
+    })
 });
+
+describe("check game over for multiple ships", () => {
+    const newPlayer = player();
+    newPlayer.board.placeShip([ [0,0] ]);
+    newPlayer.board.placeShip([ [1,1] ]);
+    newPlayer.board.placeShip([ [2,1], [2,3] ]);
+
+    newPlayer.fire([0,0]);
+
+    it("game not over", () => {
+        expect(newPlayer.isGameOver()).toBe(false);
+    })
+
+    it("game over", () => {
+        newPlayer.fire([1,1]);
+        newPlayer.fire([2,1]);
+        newPlayer.fire([2,3]);
+
+        expect(newPlayer.isGameOver()).toBe(true);
+    })
+})
